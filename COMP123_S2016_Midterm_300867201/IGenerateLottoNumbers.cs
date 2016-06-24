7dff9c921279e467000bca9719fc25f4fcc832e7
gapillan <gapillan@my.centennialcollege.ca>
2016-06-24 11:03:21 -0400

    Added a template of the Lotto file

---
+++ COMP123_S2016_Midterm_300867201/IGenerateLottoNumbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP123_S2016_Midterm_300867201
+{
+    public interface IGenerateLottoNumbers
+    {
+        void GenerateLottoNumbers();
+    }
+}
